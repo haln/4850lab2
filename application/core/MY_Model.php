e9071733bd21b5a1f58be86f0d48a3095b53076d
Kreygasm <anderson0phan@gmail.com>
2016-01-24 13:53:44 -0800

    added welcome.php controller stuff

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -33,6 +33,13 @@ class Images extends CI_Model {
 		return $query->result_array();
 	}
 
+	function newest()
+	{
+		$this->db->order_by("id", "desc");
+		$this->db->limit(3);
+		$query = $this->db->get('images');
+		return $query->result_array();
+	}
 //---------------------------------------------------------------------------
 //  Table management functions
 //---------------------------------------------------------------------------
